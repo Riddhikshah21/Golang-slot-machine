110273ae65956f1a07b696cd02aed7468e58663a
Riddhikshah21 <51504693+Riddhikshah21@users.noreply.github.com>
2025-11-02 00:00:48 -0400

    Getting bet amount from the user

--- main.go
+++ main.go
@@ -14,6 +14,30 @@ func getName() string {
 	return name
 }
 
+func get_bet(balance uint) uint {
+	var bet uint
+	for true {
+		fmt.Printf("Enter your bet, or 0 to quit (balance: $%d): ", balance)
+		fmt.Scan(&bet)
+		if bet > uint(balance) {
+			fmt.Println("Bet cannot be greater than balance amount!")
+		} else {
+			break
+		}
+	}
+	return bet
+}
+
 func main() {
+	balance := uint(200)
 	getName()
+
+	for balance > 0 {
+		bet := get_bet(balance)
+		if bet == 0 {
+			break
+		}
+		balance -= bet
+	}
+	fmt.Printf("You are left with $%d\n", balance)
 }
